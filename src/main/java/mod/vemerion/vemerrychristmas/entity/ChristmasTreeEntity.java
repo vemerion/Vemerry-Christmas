bc249893b9396af285fbb3708f32796aa2c47cb1
vemerion <vemerion@gmail.com>
2020-12-09 20:09:34 +0100

    Make ChristmasTreeEntity unmovable but attackable

--- src/main/java/mod/vemerion/vemerrychristmas/entity/ChristmasTreeEntity.java
+++ src/main/java/mod/vemerion/vemerrychristmas/entity/ChristmasTreeEntity.java
@@ -1,10 +1,19 @@
 package mod.vemerion.vemerrychristmas.entity;
 
+import mod.vemerion.vemerrychristmas.ModInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class ChristmasTreeEntity extends Entity {
@@ -15,22 +24,62 @@ public class ChristmasTreeEntity extends Entity {
 
 	@Override
 	protected void initDataTracker() {
-		
+
 	}
 
 	@Override
 	protected void readCustomDataFromTag(CompoundTag tag) {
-		
+
 	}
 
 	@Override
 	protected void writeCustomDataToTag(CompoundTag tag) {
-		
+
 	}
 
 	@Override
 	public Packet<?> createSpawnPacket() {
 		return new EntitySpawnS2CPacket(this);
+	}
+
+	@Override
+	public boolean damage(DamageSource source, float amount) {
+		if (isInvulnerableTo(source)) {
+			return false;
+		} else {
+			if (world.isClient)
+				return true;
+
+			Block.dropStack(this.world, this.getBlockPos(), new ItemStack(ModInit.CHRISTMAS_TREE_ITEM));
+			remove();
+			return true;
+		}
+	}
+
+	@Override
+	public PistonBehavior getPistonBehavior() {
+		return PistonBehavior.IGNORE;
+	}
+
+	@Override
+	protected boolean canClimb() {
+		return false;
+	}
+
+	// Determines if the entity can be interacted with
+	@Override
+	public boolean collides() {
+		return true;
+	}
+
+	@Override
+	public ActionResult interact(PlayerEntity player, Hand hand) {
+		return ActionResult.CONSUME;
+	}
+
+	@Override
+	public ActionResult interactAt(PlayerEntity player, Vec3d hitPos, Hand hand) {
+		return ActionResult.PASS;
 	}
 
 }
